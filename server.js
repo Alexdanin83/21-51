5d7997ea0eab4b1116f0985e8b64c81009dc6651
Your Name <you@example.com>
2021-02-17 18:56:39 +0100

    Fix update clients

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require('express')
-const app = express();
 const path = require('path');
 const cors = require('express-cors');
 const socket = require('socket.io');
-app.use(cors());
-app.use(
-  cors()
-);
-app.use((req, res, next) => {
-  req.io = io;
-  next();
+const app = express();
+
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
 });
+app.use(cors());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -21,12 +18,31 @@ app.use(function(req, res, next) {
   next();
 });
 
+const io = socket(server, {
+  cors: {
+    origin: '*',
+    }
+  });
+
+  //podpinamy do serwera sockety
+//  const io = socket(server);
+  io.on('connection', (socket) => {
+    //wysłanie do wybranego clienta
+    //podłączenemu clientowi odświeży się lista tasków
+    //io.to(socket.id).emit('updateData');
+    //  socket.emit('updateData', tasks);
+    console.log('New client! Its id – ' + socket.id);
+
+  });
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
 
 app.use(express.json());
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '/client/build')));
-
-
 const testimonials = require('./routes/testimonials.router');
 const concerts = require('./routes/concerts.router');
 const seats = require('./routes/seats.router');
@@ -43,18 +59,3 @@ app.get('*', (req, res) => {
 app.use((req, res) => {
   res.status(404).json({message: 'Not found...'});
 })
-
-const server = app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running on port: 8000');
-});
-//podpinamy do serwera sockety
-const io = socket(server);
-io.on('connection', (socket) => {
-  //wysłanie do wybranego clienta
-  //podłączenemu clientowi odświeży się lista tasków
-  //io.to(socket.id).emit('updateData');
-  //  socket.emit('updateData', tasks);
-  console.log('New client! Its id – ' + socket.id);
-
-
-});
